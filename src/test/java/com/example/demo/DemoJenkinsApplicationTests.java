002b4d21a06dd0de731e3e0e713b4dadb33562bd
vimalraj-tecdata <vimalraj@tecdata.co.in>
2024-12-05 18:07:20 +0530

    add void

--- src/test/java/com/example/demo/DemoJenkinsApplicationTests.java
+++ src/test/java/com/example/demo/DemoJenkinsApplicationTests.java
@@ -13,7 +13,7 @@ class DemoJenkinsApplicationTests {
 	public static Logger logger=LoggerFactory.getLogger(DemoJenkinsApplicationTests.class);
 	
 	@Test
-	 contextLoads() {
+	void contextLoads() {
 		logger.info("Test case executing.......!!!");
 		logger.info("Test case executing second log statement.......!!!");
 
